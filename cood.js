8e1b109e2a45db7271f2a0e2d3cfacce589d4acd
cympfh <cympfh@gmail.com>
2014-05-27 12:52:01 +0900

    add test

--- cood.js
+++ cood.js
@@ -1,7 +1,7 @@
 var curl = require('./curl');
 
 function search(word, cont) {
-  var url = "http://www.geocoding.jp/api/?v=1.1&q=" + word;
+  var url = "http://www.geocoding.jp/api/?v=1.1&q=" + encodeURIComponent(word);
   curl(url, "GET", function (xml) {
     var lat = xml.match(/<lat>(.*)<\/lat>/);
     lat = lat ? lat[1] : null;
@@ -16,5 +16,6 @@ function search(word, cont) {
 function test(words) {
   words.forEach(function(w){ search(w, console.log) });
 }
+// test(['本郷', '東京 本郷', '本八幡', '市川', '神保町']);
 
 module.exports = search;
