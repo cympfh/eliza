fdc989033f5bf65efd6a4f0662502bffad326804
cympfh <hattori@kanga.nii.ac.jp>
2014-12-17 08:16:57 +0900

    2014年 12月 17日 水曜日 08:16:57 JST

--- zoi.js
+++ zoi.js
@@ -79,7 +79,8 @@ var ar = [
       'pic.twitter.com/xpfgbad6bT',
       'pic.twitter.com/yh1ObvxjVI',
       'pic.twitter.com/7l0ttenH6c',
-      'pic.twitter.com/Lo18b1g3zx'
+      'pic.twitter.com/Lo18b1g3zx',
+      'pic.twitter.com/TBZk1n028O'
 ];
 
 function zoi(cont) {
